b618989617fae72c917d8db30cc8f7ccea7304d2
Alex <84599917+GyverLibs@users.noreply.github.com>
2022-02-23 10:41:00 +0300

    upd

--- src/parseUtils.cpp
+++ src/parseUtils.cpp
@@ -1,7 +1,7 @@
 #include "parseUtils.h"
 
 // количество подстрок в списке list с разделителем div
-uint8_t GP_listSize(char* list, char div = ',') {
+uint8_t GP_listSize(char* list, char div) {
     uint16_t i = 0, count = 0;
     while (list[i]) if (list[i++] == div) count++;
     return ++count;
