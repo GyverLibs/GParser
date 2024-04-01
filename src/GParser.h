441e5964a420651b4fbbe5c6f3015b52ceb97ea5
GyverLibs <alexgyvershow@ya.ru>
2024-04-01 13:46:01 +0300

    upd

--- src/GParser.h
+++ src/GParser.h
@@ -1,6 +1,6 @@
 /*
     Простой и быстрый парсер строк в отдельные подстроки и числа для Arduino
-    Документация: 
+    Документация:
     GitHub: https://github.com/GyverLibs/GParser
     Возможности:
     - Работает с указанным буфером (строкой), не создаёт свой
@@ -8,7 +8,7 @@
     - Позволяет обращаться к подстрокам, переводить их в числа и сравнивать со строками
     - Доступ к подстрокам через []
     - Может распарсить строку в массив int или byte
-    
+
     AlexGyver, alex@alexgyver.ru
     https://alexgyver.ru/
     MIT License
@@ -24,77 +24,84 @@
 
 #ifndef _GParser_h
 #define _GParser_h
-#include "parseUtils.h"
-#include "unicode.h"
-#include "url.h"
+#include "utils/parseUtils.h"
+#include "utils/unicode.h"
+#include "utils/url.h"
 
 class GParser {
-public:
+   public:
     // передать char array строку, можно указать символ разделитель
-    GParser (char* data, char newDiv = ',') {
+    GParser(char* data, char newDiv = ',') {
         buf = data;
         div = newDiv;
     }
     ~GParser() {
         clear();
     }
-    
+
     // указать символ разделитель
     void setDiv(char newDiv) {
         div = newDiv;
     }
-    
+
     // освободить буфер
     void clear() {
-        if (str) free(str);
+        if (str) delete[] str;
+        str = nullptr;
     }
-    
+
     // количество разделённых данных в пакете
     int amount() {
-        int i = 0, count = 0;
-        while (buf[i]) if (buf[i++] == div) count++;  // подсчёт разделителей
-        return ++count;
+        if (!buf) return 0;
+        int count = 1;
+        char* p = buf;
+        while (*p) {
+            if (*p++ == div) count++;  // подсчёт разделителей
+        }
+        return count;
     }
-    
+
     // разделить строку на подстроки
     int split() {
-        len = amount();                     // количество данных
-        clear();                            // освобождаем буфер
-        str = (char**)malloc(len * sizeof(char*)); // создаём буфер
-        str[0] = buf;                       // строка 0
-        int i = 0, j = 0;                   // счётчики
-        while (buf[i]) {                    // пока не NULL
-            if (buf[i] == div) {            // если разделитель
-                buf[i] = '\0';              // меняем на NULL
-                str[++j] = buf + i + 1;     // запоминаем начало строки
+        len = amount();                  // количество данных
+        clear();                         // освобождаем буфер
+        str = new char*[len];            // создаём буфер
+        if (!str) return 0;              // ошибка аллокации
+        str[0] = buf;                    // строка 0
+        int i = 0, j = 0;                // счётчики
+        while (buf[i]) {                 // пока не NULL
+            if (buf[i] == div) {         // если разделитель
+                buf[i] = '\0';           // меняем на NULL
+                str[++j] = buf + i + 1;  // запоминаем начало строки
             }
             i++;
         }
         return len;
     }
-    
+
     // восстановить строку (вернуть разделители)
     void restore() {
         for (int i = 0; i < len - 1; i++) str[i][strlen(str[i])] = div;
     }
-    
+
     // получить инт из выбранной подстроки
     int32_t getInt(int num) {
         return atol(str[num]);
     }
-    
+
     // получить float из выбранной подстроки
     float getFloat(int num) {
         return atof(str[num]);
     }
-    
+
     // сравнить подстроку с другой строкой
     bool equals(int num, const char* comp) {
         return !strcmp(str[num], comp);
     }
-    
+
     // распарсить на лонги
     int parseLongs(long* data) {
+        if (!buf) return 0;
         int count = 0;
         char* offset = buf;
         while (true) {
@@ -105,9 +112,10 @@ public:
         }
         return count;
     }
-    
+
     // распарсить на инты
     int parseInts(int* data) {
+        if (!buf) return 0;
         int count = 0;
         char* offset = buf;
         while (true) {
@@ -118,9 +126,10 @@ public:
         }
         return count;
     }
-    
+
     // распарсить на байты
     int parseBytes(uint8_t* data) {
+        if (!buf) return 0;
         int count = 0;
         char* offset = buf;
         while (true) {
@@ -132,14 +141,14 @@ public:
         return count;
     }
 
-    char* buf = NULL;
-    char** str = NULL;
+    char* buf = nullptr;
+    char** str = nullptr;
 
-    char* operator [] (uint16_t idx) {
+    char* operator[](uint16_t idx) {
         return str[idx];
-    }    
-    
-private:
+    }
+
+   private:
     char div;
     int len;
 };
